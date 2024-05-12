4a09e07979d7332c938a61585d0d38163935df7b
AlejandroC05 <yeineralejandro3@gmail.com>
2024-05-12 16:36:41 -0500

    Continuacion del proyecto

--- constants.js
+++ constants.js
@@ -4,11 +4,13 @@ const DB_HOST = "apirest.cqnbafc.mongodb.net"
 
 const API_VERSION = "v1"
 const IP_SERVER = "localhost"
+const JWT_SECRET_KEY = "l89idsjfjsdkl34lksldm3e5sd9uiaj23fgzkkjfsdt"
 
 module.exports = {
     DB_USER,
     DB_PASSWORD,
     DB_HOST,
     API_VERSION,
-    IP_SERVER
+    IP_SERVER,
+    JWT_SECRET_KEY
 }
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcryptjs")
 const User = require("../models/user.model")
+const jwt = require("../utils/jwt")
+const { accepts } = require("express/lib/request")
 
 function register(req, res){ 
     const {firstname, lastname, email, password} = req.body
@@ -32,6 +34,55 @@ function register(req, res){
     })
 }
 
+function login(req, res){
+    const { email, password } = req.body
+    if(!email) res.status(400).send({msg: "El email es obligatorio"})
+    if(!password) res.status(400).send({msg: "La contraseña es obligatoria"})
+    
+    const emailLowerCase = email.toLowerCase()
+
+    User.findOne({ email: emailLowerCase}, (error, userStorage) => {
+        if(error){
+            res.status(500).send({msg: "Error del servidor"})
+        } else {
+            bcrypt.compare(password, userStorage.password, (bcryptError, check) => {
+                if (bcryptError){
+                    res.status(500).send({msg: "Error del servidor"})
+                } else if (!check) {
+                    res.status(400).send({msg: "Usuario o contraseña incorrecta"})
+                } else if (!userStorage.active) {
+                    res.status(400).send({msg: "Usuario no autorizado o no activo"})
+                } else {
+                    res.status(200).send({
+                        access: jwt.createAccessToken(userStorage),
+                        refresh: jwt.createRefreshToken(userStorage)
+                    })
+                }
+            })
+        }
+    })
+}
+
+function refreshAccessToken(req, res){
+    const { token } = req.body
+
+    if(!token) res.status(400).send({msg: "Error token requerido"})
+
+    const { user_id } = jwt.decoded(token)
+
+    User.findOne({ _id: user_id}, (error, userStorage) => {
+        if(error){
+            res.status(500).send({msg: "Error del servidor"})
+        } else {
+            res.status(200).send({
+                accessToken: jwt.createAccessToken(userStorage)
+            })
+        }
+    })
+}
+
 module.exports = {
     register,
+    login, 
+    refreshAccessToken
 }
